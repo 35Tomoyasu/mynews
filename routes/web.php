afc7ae098f0b0bf667508ac03c8e651758e1c6f1
Tomoyasu <t.shirayama1109@gmail.com>
2020-11-03 02:09:08 +0000

    最新

--- routes/web.php
+++ routes/web.php
@@ -15,41 +15,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create',
-'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin' , 'middleware' => 'auth'], function()
+{
+    Route::get('news/create','Admin\NewsController@add');
+    Route::post('news/create','Admin\NewsController@create');
 });
 
 
+// PHP/Laravel 13 ニュース投稿画面を作成しよう　課題3,6で編集
 
-//以下より課題
-?>
-
-
-<?php
-
-/*
-3.「http://XXXXXX.jp/XXX というアクセスが来たときに、
-AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください
-*/
-
-Route::get('XXX',
-'Admin\AAAController@bbb');
-
-/*
-4. 【応用】
-前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。
-web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の
-add Action に、admin/profile/edit にアクセスしたら ProfileController の
-edit Action に割り当てるように設定してください。
-*/
-
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/create',
-'Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit',
-'Admin\ProfileController@edit')->middleware('auth');
+Route::group(['prefix' => 'admin' , 'middleware' => 'auth'], function()
+{
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::post('profile/create','Admin\ProfileController@create');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::post('profile/edit','Admin\ProfileController@update');
 });
+
 
 
 
